1266fda3d84e193c73f066316dd434390ce425fe
Henry Darana <47526593+Henry-Darana@users.noreply.github.com>
2023-06-19 10:31:53 +0530

    Update HelloController.java

--- src/main/java/com/course/tomcat/controller/HelloController.java
+++ src/main/java/com/course/tomcat/controller/HelloController.java
@@ -18,10 +18,4 @@ public class HelloController {
     int random(){
         return ThreadLocalRandom.current().nextInt(0,1000);
     }
- 
-    @GetMapping(value = "/hero")
-    String hero(){
-        return "My favorite superhero is Batman";
-    }
- 
 }
